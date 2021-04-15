9df3f79b5b3bbf7a2466cc30c6419d98cfaa51c6
Baldip-Singh <baldip.singh18@stjohns.edu>
2021-04-15 08:31:20 -0400

    First Push

---
+++ InterviewPrep/src/ReverseJavaString.java
@@ -0,0 +1,12 @@
+
+
+//How to reverse a String in Java?
+
+public class ReverseJavaString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ InterviewPrep/src/SwapTwoNumbers.java
@@ -0,0 +1,6 @@
+
+
+//How to swap two numbers without using a third variable?
+public class SwapTwoNumbers {
+
+}
